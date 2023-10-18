7a893acb371c75b1ba06f01c7674823bfadbc6f1
Akash <akashindia572@gmail.com>
2023-10-18 23:47:38 +0530

    Learning about FTELL function in FIleHandling in C

---
+++ FtellFuncton_Filehandling.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+    FILE *fp= NULL;
+    char str[50];
+
+    fp=fopen("NewTest.txt", "r");
+
+    if(fp==NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+
+    printf("%d",ftell(fp)); // Should Print 0 as starting of file
+
+    fscanf(fp,"%s",str);
+    printf("\n%s",str);     //Should print the string My according to the file.
+
+
+    fseek(fp,0,SEEK_END);   // Offset 0 to end of file = Lenfth of file.
+    printf("\n %d",ftell(fp));
+
+    fclose(fp);
+}
